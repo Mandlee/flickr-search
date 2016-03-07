1503fde0059468b5b67459948195e3834289b12f
BenceMandli <bence.mandli@centricdigital.com>
2016-03-07 12:54:27 +0100

    Improve the single photo page, and add little animate on home screen

--- app/controller/PhotoController.js
+++ app/controller/PhotoController.js
@@ -3,21 +3,24 @@
  */
 'use strict';
 
-app.controller('PhotoController', ['$scope', '$stateParams', 'HttpService', function ($scope, $stateParams, HttpService) {
+app.controller('PhotoController', ['$scope', '$stateParams', 'HttpService', '$state', function ($scope, $stateParams, HttpService, $state) {
     var vm = this;
-
-    console.log($stateParams.id);
 
     HttpService.get("/rest", {
         method: 'flickr.photos.getInfo',
         photo_id: $stateParams.id
-        //secret: secret,
     }).success(function (data) {
         console.log(data);
         $scope.picture = data.photo;
     }).error(function (error) {
         console.error(error);
     });
+
+    vm.selectTag = function (tag) {
+        $state.go('main.search', {
+            tag: tag
+        });
+    };
 
 }
 ]);
